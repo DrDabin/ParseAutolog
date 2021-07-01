3b5d839617d33e5f4021d9d8bc98c52bd589bbc1
DrDabin <Aibolit198@mail.ru>
2021-07-01 21:18:12 +0400

    fix fail resurs, fix includ 7z, manifest

---
+++ includeParse/Version.h
@@ -0,0 +1,12 @@
+﻿#ifndef VersionH
+#define VersionH
+
+#define MY_VER_MAJOR 1
+#define MY_VER_MINOR 0
+#define MY_VER_BUILD 0
+#define MY_VERSION "1.0.0\000"
+#define MY_DATE "2021.06.30\000"
+#define MY_AUTHOR_NAME "Alex\000"
+#define MY_COPYRIGHT "(c) Alex1983 "  MY_DATE
+
+#endif
